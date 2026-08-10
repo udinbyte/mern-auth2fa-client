98822f3aeac6142f27cbc43fa1ef86c28b689fb8
udinbyte <udincode71@gmail.com>
2026-08-10 20:12:16 +0700

    Initial commit from Acode

---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import QueryProvider from "./context/query-provider.tsx";
+import { ThemeProvider } from "./context/theme-provider.tsx";
+import { Toaster } from "./components/ui/toaster.tsx";
+
+const url = import.meta.env.VITE_API_BASE_URL || "url kosong";
+console.warn(url);
+
+import eruda from "eruda"
+eruda.init()
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryProvider>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App />
+        <Toaster />
+      </ThemeProvider>
+    </QueryProvider>
+  </StrictMode>
+);
